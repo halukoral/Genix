ebbb982406408735a562639a487bb2a0c22db7e1
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-03-03 06:55:54 +0300

    Update glad's premake file

--- Genix/ThirdParty/glad/premake5.lua
+++ Genix/ThirdParty/glad/premake5.lua
@@ -1,7 +1,8 @@
 project "glad"
     kind "StaticLib"
     language "C"
-
+    staticruntime "on"
+    
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
@@ -12,14 +13,18 @@ project "glad"
         "src/glad.c"
     }
 
-	includedirs
-	{
-		"include"
-	}
+    includedirs
+    {
+        "include"
+    }
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
