2890f7b84b1cde8277d5e7b9b3c8a9896905bae5
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-03-10 18:12:48 +0300

    Avoid using std::function in event dispatcher for performance

--- Genix/src/Genix/Events/Event.h
+++ Genix/src/Genix/Events/Event.h
@@ -78,15 +78,13 @@ class EventDispatcher
 public:
 	EventDispatcher(Event& event) : m_Event(event) { }
 
-	template<typename T>
-	using EventFn = std::function<bool(T&)>;
-
-	template<typename T>
-	bool Dispatch(EventFn<T> func)
+	// F will be deduced by the compiler
+	template<typename T, typename F>
+	bool Dispatch(const F& func)
 	{
 		if (m_Event.GetEventType() == T::GetStaticType())
 		{
-			m_Event.m_Handled = func(*(T*)&m_Event);
+			m_Event.m_Handled = func(static_cast<T&>(m_Event));
 			return true;
 		}
 		return false;
