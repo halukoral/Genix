fd433a902fbdbf6ce5e4101f7704c10f44d3f705
Haluk Oral <67242728+halukoral@users.noreply.github.com>
2024-03-18 07:17:07 +0300

    Added check for OpenGL version

--- Genix/src/Genix/Platform/OpenGL/OpenGLContext.cpp
+++ Genix/src/Genix/Platform/OpenGL/OpenGLContext.cpp
@@ -21,6 +21,15 @@ void OpenGLContext::Init()
 	LOG_CORE_INFO("  Vendor: {0}",  (const char*)glGetString(GL_VENDOR));
 	LOG_CORE_INFO("  Renderer: {0}",  (const char*)glGetString(GL_RENDERER));
 	LOG_CORE_INFO("  Version: {0}",  (const char*)glGetString(GL_VERSION));
+
+#ifdef GX_ENABLE_ASSERTS
+	int versionMajor;
+	int versionMinor;
+	glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+	glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+	ASSERT_CORE(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Genix requires at least OpenGL version 4.5!")
+#endif
 }
 
 void OpenGLContext::SwapBuffers()
